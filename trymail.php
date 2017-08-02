1b031d1862af000431ae8f1e33b9f62de14a8eaf
cocostru <dk.pochtamp@gmail.com>
2017-08-02 19:45:41 +0800

    new inmrkt mail debug

--- trymail.php
+++ trymail.php
@@ -13,7 +13,7 @@ $message = 'Hello!';
 
 // To send HTML mail, the Content-type header must be set
 $headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+// $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
 // Additional headers
 $headers .= 'To: Denis <dk.pochtamp@gmail.com>' . "\r\n";
